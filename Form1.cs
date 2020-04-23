7cda4196a1a94848887770fb97892afa27ef7e6c
George Predoiu <63638651+george-jpg@users.noreply.github.com>
2020-04-24 01:10:22 +0300

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -12,49 +12,76 @@ namespace AudioPlayerApp
 {
     public partial class AudioPlayerApp : Form
     {
+        private Mp3Player mp3Player = new Mp3Player();
+
         public AudioPlayerApp()
         {
             InitializeComponent();
         }
+        //Create Global Variables of String Type Array to save  the titles  of the Tracks  and path of the track
+        String[] paths, files;
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-        //Create Global Variables pf String Type Array to save the titles or name of the Tracks and path of the track
-        String[] paths, files;
-
-        private void buttonSelectSongs_Click(object sender, EventArgs e)
-        {
-            // Code to Select Songs
-            OpenFileDialog ofd = new OpenFileDialog();
-            //Code to select multiple songs
-            ofd.Multiselect = true;
-            if(ofd.ShowDialog()==System.Windows.Forms.DialogResult.OK)
-            {
-                files = ofd.SafeFileNames; //Save the names of the track in files array
-                paths = ofd.FileNames; // Save the paths of the tracks in path array
-                //Display the music titles in listbox
-
-            for (int i=0; i < files.Length; i++)
-                {
-                    listBoxSongs.Items.Add(files[i]); //Display Songs in ListBox
-
-                }
-            }
-        }
-
-        private void listBoxSongs_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            //Write a code to play music
-            axWindowsMediaPlayerMusic.URL = paths[listBoxSongs.SelectedIndex];
-        }
+  
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             //Code to closer the app
 
             Environment.Exit(1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {// Code to Select Songs
+            using (OpenFileDialog ofd = new OpenFileDialog())
+
+            {
+                ofd.Filter = "Mp3 Files|*.mp3";
+
+                //Code to Select Multiple Songs
+                ofd.Multiselect = true;
+
+                if  (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+
+                {    
+                    files = ofd.SafeFileNames; //Save the names of the track in filles array
+                    paths = ofd.FileNames; //Save the paths of the tracks  in path array 
+                    mp3Player.open(ofd.FileName);
+
+                    // Dispay  the music  titles in listbox
+
+                    for  (int i =0; i < files.Length; i++)
+                    {
+
+                        listBoxSongs.Items.Add(files[i]); //Display songs in Listbox
+                    }
+
+             
+                }
+
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        { // Code to play music
+            mp3Player.play();
+
+        }
+
+        private void listBoxSongs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {// code to stop music
+            mp3Player.stop();
+
         }
     }
 }
